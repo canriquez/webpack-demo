e64a538e44b6b6fb1e9220ecd63a50f7e32a4865
Carlos Anriquez <canriquez@gmail.com>
2020-06-28 10:37:50 -0300

    adding print.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,18 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './pizza.jpg';
-import Data from './data.xml';
+import printMe from './print.js';
+
 
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
 
-    // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
 
-    // Add the image to our existing div.
-    const myIcon = new Image();
-    myIcon.src = Icon;
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe(btn);
 
-    element.appendChild(myIcon);
-    console.log(Data);
+    element.appendChild(btn);
     return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,76 +1,36 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js',
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+        }),
+    ],
     output: {
-        filename: 'bundle.js',
+
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
     module: {
         rules: [
             {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                ],
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader',
-                ],
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    'file-loader',
-                ],
-            },
-            {
-                test: /\.(csv|tsv)$/,
-                use: [
-                    'csv-loader',
-                ],
-            },
-            {
-                test: /\.xml$/,
-                use: [
-                    'xml-loader',
-                ],
-            },
-            /*             {
-                            test: /\.(gif|png|jpe?g|svg)$/i,
-                            use: [
-                                'file-loader',
-                                {
-                                    loader: 'image-webpack-loader',
-                                    options: {
-                                        bypassOnDebug: true, // webpack@1.x
-                                        disable: true, // webpack@2.x and newer
-                                        mozjpeg: {
-                                            progressive: true,
-                                            quality: 90
-                                        },
-                                        // optipng.enabled: false will disable optipng
-                                        optipng: {
-                                            enabled: false,
-                                        },
-                                        pngquant: {
-                                            quality: [0.65, 0.90],
-                                            speed: 4
-                                        },
-                                        gifsicle: {
-                                            interlaced: false,
-                                        },
-                                        // the webp option will enable WEBP
-                                        webp: {
-                                            quality: 75
-                                        },
-                                    },
-                                },
-                            ],
-                        }, */
-        ],
-    },
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
 };
